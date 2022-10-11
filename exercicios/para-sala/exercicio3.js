9ad9899982722b3548c09673ac4b41bce02c2052
Xênia Barreto <xenia.barreto@queimadiaria.com.br>
2022-10-10 21:01:11 -0300

    update homework

--- exercicios/para-sala/exercicio3.js
+++ exercicios/para-sala/exercicio3.js
@@ -1,11 +1,3 @@
-/* Crie uma classe chamada Author que extenderá de User as informações de email e password e 
-terá um atributo privado com o número de post inicializando com 0. 
-
-Essa classe terá dos metódos, um para criar o post que incrementará a quantidade de post e 
-o outro obter o número de post criados. 
-
-Observação: Não criaremos a implementação da adição do post; */
-
 class User {
 
     #password;
