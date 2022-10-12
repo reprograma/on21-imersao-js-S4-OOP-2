af191a013bf3fa397728075ebcee2b3b924d203f
Ana Paula Lange Gomes <anaplg@pbtech.net.br>
2022-10-12 20:06:19 -0300

    correcao classes

--- exercicios/para-casa/entregas/anapaula-langegomes/Book.js
+++ exercicios/para-casa/entregas/anapaula-langegomes/Book.js
@@ -5,12 +5,14 @@ export class Book extends Author {
     #isbn
     #price
     #qty
-    constructor(authorsName, booksName, isbn, price, qty) {
-        super(authorsName)
-        this.#booksName = booksName
-        this.#isbn = isbn
-        this.#price = price
-        this.#qty = qty
+
+constructor(authorsName, booksName, isbn, price, qty) {
+    super(authorsName)
+    this.#booksName = booksName
+    this.#isbn = isbn
+    this.#price = price
+    this.#qty = qty
+    
     }
 
 }
