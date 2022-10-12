8e3227855875855a1b3b1bcf5d93cde2825ee6ce
Mariana Trombini <mariana.trombini@dasa.com.br>
2022-10-11 23:39:48 -0300

    :tada: feat: add book class with its attributes, getters, setters and  methods

--- exercicios/para-casa/entregas/mariana-trombini/book.js
+++ exercicios/para-casa/entregas/mariana-trombini/book.js
@@ -2,47 +2,79 @@ const Author = require('./author')
 
 class Book extends Author {
 
-  #name
+  #title
   #isbn
   #price
   #quantity
 
-  constructor(name, isbn, author, price, quantity){
-    super(author)
-    this.#name = name
+  constructor(title, isbn, name, price, quantity){
+    super(name)
+    this.#title = title
     this.#isbn = isbn
     this.#price = price
     this.#quantity = quantity
   }
 
-  getName(){
-    return `Book's title: ${this.#name}`
+ get title(){
+  return this.#title
+ }
+
+  getTitle(){
+    return `Book's title: ${this.title}`
+  }
+
+  get isbn(){
+    return this.#isbn
   }
 
   getIsbn(){
-    return `Book's ISBN: ${this.#isbn}`
+    return `Book's ISBN: ${this.isbn}`
+  }
+
+  get name(){
+    return this._name
   }
 
   getAuthor(){
     return `Author's name: ${this.name}`
   }
 
+  get price(){
+    return this.#price
+  }
+
   getPrice(){
     return `Book's price: ${this.#price}`
+  }
+
+  get quantity(){
+    return this.#quantity
   }
 
   getQuantity(){
     return `Book's quantity: ${this.#quantity}`
   }
 
-  setName(newName){
-    this.#name = newName
-    return `Name changed successfully. New book's name: ${this.#name}`
+  set title(newTitle){
+    return this.#title = newTitle
+  }
+
+  setTitle(newTitle){
+    this.title = newTitle
+    return `Name changed successfully. New book's name: ${this.#title}`
+  }
+
+  set isbn(newIsbn){
+    return this.#isbn = newIsbn
   }
 
   setIsbn(newIsbn){
-    this.#isbn = newIsbn
-    return `ISBN changed successfully. New book's ISBN: ${this.#isbn}`
+    this.isbn = newIsbn
+    return `ISBN changed successfully. New book's ISBN: ${this.isbn}`
+  }
+
+  set name(newName){
+    return this._name = newName
   }
 
   setAuthor(newAuthor){
@@ -50,14 +82,26 @@ class Book extends Author {
     return `Author's name changed successfully. New book's author: ${this.name}`
   }
 
+  set price(newPrice){
+    return this.#price = newPrice
+  }
+
   setPrice(newPrice){
-    this.#price = newPrice
-    return `Price changed successfully. New book's price: ${this.#price}`
+    this.price = newPrice
+    return `Price changed successfully. New book's price: ${this.price}`
+  }
+
+  set quantity(newQuantity){
+    return this.#quantity = newQuantity
   }
 
   setQuantity(newQuantity){
-    this.#quantity = newQuantity
-    return `Quantity changed successfully. New book's quantity: ${this.#quantity}`
+    this.quantity = newQuantity
+    return `Quantity changed successfully. New book's quantity: ${this.quantity}`
+  }
+
+  printBook(){
+    return `--- Book --- \nTitle: ${this.title}, \nISBN: ${this.isbn}, \nAuthor: ${this.name}, \nPrice: R$${this.price}, \nQuantity: ${this.quantity}`
   }
 }
 
