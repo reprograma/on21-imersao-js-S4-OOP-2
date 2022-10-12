c814b09727854b2c25e665bfd92f82add03702c0
Fernanda Soares <fernanda.soares@elogroup.com.br>
2022-10-11 21:12:55 -0300

    add ex 4

---
+++ exercicios/para-sala/exercicio4/User.js
@@ -0,0 +1,57 @@
+class User {
+  email;
+  nome;
+  #senha;
+
+  constructor() {}
+
+  signUp(nome, email, senha) {
+    let isValido = this.#validarEmail(email);
+
+    if (isValido) {
+      this.nome = nome;
+      this.email = email;
+      this.#senha = senha;
+      return "Usuário criado!";
+    } else return "Email inválido!";
+  }
+
+  //método privado
+  #validarEmail(email) {
+    if (email.includes("@")) {
+      return true;
+    }
+    return false;
+  }
+
+  login(email, senha) {
+    if (email === this.email && senha === this.#senha)
+      return "Login realizado!";
+
+    return "Falha no login!";
+  }
+
+  alteraSenha(senhaAtual, senhaNova) {
+    if (senhaAtual === this.#senha) {
+      this.#senha = senhaNova;
+      return "Senha alterada!";
+    }
+
+    return "Falha na alteração!";
+  }
+
+  get senha() {
+    return `A senha é ${this.#senha}`;
+  }
+
+  set senha(senha) {
+    this.#senha = senha;
+  }
+
+  get email() {
+    return this.email;
+  }
+}
+
+const newUser = new User();
+console.log(newUser.signUp("fer", "fer@email.com", "12345"));
