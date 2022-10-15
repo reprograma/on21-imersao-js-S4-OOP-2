193cab57156339dfb63cfefd1693b2f84d23a955
rosanaemidio <rosanaemidio42@gmail.com>
2022-10-14 21:46:02 -0300

    feat: adicionado resolução dos exercicios

---
+++ exercicios/para-casa/entregas/rosana-emidio/Author.js
@@ -0,0 +1,40 @@
+class Author{
+    #name
+    #email
+    #birthdate
+
+    constructor(name, email, birthdate){
+        this.#name = name;
+        this.#email = email;
+        this.#birthdate = birthdate;
+    }
+
+    get name(){
+        return this.#name;
+    }
+
+    get email() {
+        return this.#email;
+    }
+
+    get birthdate() {
+        return this.#birthdate;
+    }
+
+    set name(newName) {
+        return this.#name = newName;
+    }
+
+    set email(newEmail) {
+        return this.#email = newEmail;
+    }
+
+    set birthday(newBirthdate) {
+        return this.#birthdate = newBirthdate;
+    }
+}
+module.exports = Author;
+
+const novoAutor = new Author('Hermione', 'mione@hogwarts.com', '02/10/1989')
+console.log(novoAutor);
+console.log(novoAutor.name);
